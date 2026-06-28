ffe4f1ea9f0c019d1d704e24a9f88cf4f63bb69d
HazyLunar <erikf08@gmail.com>
2026-06-27 17:40:36 -0700

    Hotfix on rogueai logic

--- data/scripts/events/cc_rogueaiprobe.lua
+++ data/scripts/events/cc_rogueaiprobe.lua
@@ -23,7 +23,6 @@ function RogueAIProbe.spawn()
     local scale = math.max(1, (500 - d) / 100)
 
     probe.damageMultiplier = scale * 5.0
-    probe.shieldMultiplier = scale * 2.0
 
     -- If it isn't killed in 3 minutes, it warps away
     probe:addScript("data/scripts/entity/story/warpawaytimer.lua", 180)
